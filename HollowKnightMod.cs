646f4eba757e901aecbc2fe20192295b93e0dc45
Alphi999 <cookayterraria@gmail.com>
2018-08-05 23:53:46 +0400

    please don't break the entire mod please

--- HollowKnightMod.cs
+++ HollowKnightMod.cs
@@ -2,10 +2,20 @@ using Terraria.ModLoader;
 
 namespace HollowKnightMod
 {
-	class HollowKnightMod : Mod
+	public class HollowKnightMod : Mod
 	{
 		public HollowKnightMod()
 		{
 		}
-	}
+        public override void Load()
+        {
+        OriginalLifeCrystalTexture = Main.itemTexture[29];
+        Main.itemTexture[29] = this.GetTexture("Textures/Mask_Shard");
+        }
+        public static Texture2D OriginalLifeCrystalTexture;
+        public override void Unload()
+        {
+        Main.itemTexture[29] = OriginalLifeCrystalTexture;
+        }
+    }
 }
