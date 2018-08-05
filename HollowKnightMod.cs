0698a9b4c29bffcefb4324bc1218ab8b876b59ca
Alphi999 <cookayterraria@gmail.com>
2018-08-06 00:29:49 +0400

    urgh finally fixed an annoying thingy

--- HollowKnightMod.cs
+++ HollowKnightMod.cs
@@ -1,10 +1,16 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace HollowKnightMod
 {
 	public class HollowKnightMod : Mod
 	{
-		public HollowKnightMod()
+        public static Texture2D OriginalLifeCrystalTexture;
+
+        public HollowKnightMod()
 		{
 		}
         public override void Load()
@@ -12,7 +18,6 @@ namespace HollowKnightMod
         OriginalLifeCrystalTexture = Main.itemTexture[29];
         Main.itemTexture[29] = this.GetTexture("Textures/Mask_Shard");
         }
-        public static Texture2D OriginalLifeCrystalTexture;
         public override void Unload()
         {
         Main.itemTexture[29] = OriginalLifeCrystalTexture;
