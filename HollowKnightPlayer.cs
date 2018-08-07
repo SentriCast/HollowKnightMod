4fc8e9291d3ae0c9ecfcbe4c7ac077d6a67a87bf
Sentri <sentricast@gmail.com>
2018-08-07 13:37:20 +0300

    setstartupinv
    yeah

---
+++ HollowKnightPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace HollowKnightMod
+{
+    class HollowKnightPlayer : ModPlayer
+    {
+        public override void SetupStartInventory(IList<Item> items)
+        {
+            player.inventory[1].SetDefaults(mod.ItemType("OldNail"));
+        }
+    }
+}
